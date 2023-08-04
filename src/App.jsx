e06bf1de73c1f5a3cb9617982afb8a2d4f675711
NatanaelJose <josesaraivamaciel@gmail.com>
2023-08-03 21:02:14 -0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function App() {
+
+
+  return <p className='bg-red-600'>pru</p>
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Helvetica Neue", "Arial", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
