3dc81f668969d29f688cfae4909154997fff63ac
NatanaelJose <josesaraivamaciel@gmail.com>
2023-08-07 19:48:52 -0300

    for you

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,19 @@
+function forYou() {
+    return (
+        <div className="flex flex-col">
+            <h2>Home</h2>
+            <div>
+                <button>For You</button>
+                <button>Following</button>
+            </div>
+        </div>
+    )
+}
+
 export default function Feed() {
     return (
         <>
-            <div className="w-2/4 h-screen bg-slate-800 ">
+            <div className="w-2/4 h-screen bg-slate-100">
             </div>
         </>
     )
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,26 +2,23 @@ import { FaTwitter, FaHome, FaUser } from 'react-icons/fa';
 
 const NavItem = ({ icon, title }) => {
     return (
-        <div className='flex items-center mb-3 transition-transform duration-300 ease-in-out transform hover:scale-125 hover:text-blue-500 text-gray-900'>
+        <div className='flex items-center cursor-pointer mb-3 transition-transform duration-300 ease-in-out transform hover:scale-125 hover:text-sky-600 text-stone-800'>
             {icon}
-            <h2 className='flex items-center w-full pl-5 text-start'>{title}</h2>
+            <h2 className='flex items-center w-full pl-5 text-start font-bold'>{title}</h2>
         </div>
     );
 };
 
 export default function Menu() {
     return (
-
         <div className="w-1/4 h-screen bg-white opacity-7 flex flex-col pt-4 pl-12 border-r-2 border-r-gray-200">
             <div className="h-106 w-full">
-                <FaTwitter size={48} className='icon text-blue-600' />
+                <FaTwitter size={48} className='text-sky-600' />
             </div>
-            <nav className='mt-10'>
-                <NavItem icon={<FaHome size={36} />} title="Home" />
-                <NavItem icon={<FaTwitter size={36} />} title="Twitter" />
-                <NavItem icon={<FaUser size={36} />} title="Profile" />
+            <nav className='mt-10 ml-2'>
+                <NavItem icon={<FaHome size={50} className="mr-3" />} title="Home" />
+                <NavItem icon={<FaUser size={38} className="mr-3" />} title="Profile" />
             </nav>
         </div>
-
-    )
+    );
 }
