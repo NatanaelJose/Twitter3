04ecab95c8ed9627afedb0d42b95238b920e7c7d
NatanaelJose <josesaraivamaciel@gmail.com>
2023-08-07 21:44:20 -0300

    for you

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,16 @@
-function forYou() {
+import { Link } from "react-router-dom"
+
+const Button = (title) => {
+    return (<button className="w-full h-14 hover:bg-slate-200 text-base"><Link>{title}</Link></button>
+    )
+}
+const ForYou = () => {
     return (
-        <div className="flex flex-col">
-            <h2>Home</h2>
-            <div>
-                <button>For You</button>
-                <button>Following</button>
+        <div className="flex flex-col w-full h-15 bg-white border-b border-r border-r-gray-200">
+            <h2 className="font-semibold text-lg text-stone-800 mb-4 p-4 pb-0">Home</h2>
+            <div className="flex flex-row justify-around">
+                <Button title="For You" />
+                <Button title="Following" />
             </div>
         </div>
     )
@@ -13,7 +19,8 @@ function forYou() {
 export default function Feed() {
     return (
         <>
-            <div className="w-2/4 h-screen bg-slate-100">
+            <div className="w-2/4 h-screen bg-slate-100 flex flex-col">
+                <ForYou />
             </div>
         </>
     )
