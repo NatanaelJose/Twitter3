9ae8d323fded33b57d266ad09ae7e0c557821f1b
NatanaelJose <josesaraivamaciel@gmail.com>
2023-08-07 22:26:58 -0300

    border For you

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom"
 
 const Button = ({ title }) => {
-    return (<button className="w-full h-14 hover:bg-slate-200 text-base"><Link>{title}</Link></button>
+    return (<button className="border-b-sky-700 border-b-4 w-full h-14 focus:font-semibold transition duration-500 ease-in-out hover:bg-slate-200 text-base"><Link>{title}</Link></button>
     )
 }
 const ForYou = () => {
     return (
-        <div className="flex flex-col w-full h-15 bg-white border-b border-r border-r-gray-200">
+        <div className="flex flex-col fixed w-2/4 h-15 bg-white border-b border-r border-r-gray-200">
             <h2 className="font-semibold text-lg text-stone-800 mb-4 p-4 pb-0">Home</h2>
             <div className="flex flex-row justify-around">
                 <Button title="For You" />
